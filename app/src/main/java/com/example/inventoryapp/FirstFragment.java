274e543f5a5ed3855b83c1827f0beff85afcf715
Bryan1102 <molnar87gabor@gmail.com>
2023-12-02 16:23:02 +0100

    toast

--- app/src/main/java/com/example/inventoryapp/FirstFragment.java
+++ app/src/main/java/com/example/inventoryapp/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,7 +12,8 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.inventoryapp.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment
+{
 
     private FragmentFirstBinding binding;
 
@@ -19,23 +21,38 @@ public class FirstFragment extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-    ) {
+    )
+    {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.buttonRnd.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+        binding.toastButton.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View view)
+           {
+               Toast toast = Toast.makeText(getActivity(), "test", Toast.LENGTH_SHORT);
+               toast.show();
+           }
+
+       });
+
     }
 
     @Override
