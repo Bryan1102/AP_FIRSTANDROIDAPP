64788075fb29573d3445ec7548157b94d47375d1
Bryan1102 <molnar87gabor@gmail.com>
2023-12-03 15:52:39 +0100

    finished this

--- app/src/main/java/com/example/inventoryapp/SecondFragment.java
+++ app/src/main/java/com/example/inventoryapp/SecondFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.inventoryapp.databinding.FragmentSecondBinding;
+
+import java.util.Random;
+
 
 public class SecondFragment extends Fragment {
 
@@ -26,8 +30,25 @@ public class SecondFragment extends Fragment {
 
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
         super.onViewCreated(view, savedInstanceState);
+
+        int counter = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        String countText = getString(R.string.lorem_ipsum, counter);
+        TextView headerView = view.getRootView().findViewById(R.id.textview_second);
+        headerView.setText(countText);
+
+
+        Random random = new java.util.Random();
+        Integer randomNumber = 0;
+        if (counter > 0)
+        {
+            randomNumber = random.nextInt(counter + 1);
+        }
+
+        TextView randomView = view.getRootView().findViewById(R.id.textView_random);
+        randomView.setText(randomNumber.toString());
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
